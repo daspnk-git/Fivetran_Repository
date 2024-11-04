89510838b9f238b9f159b8cbe197573f87d0512b
Pinaki Das <pinaki.fiem@gmail.com>
2024-11-04 13:56:28 +0000

    ..

--- models/Practise/birthdate.sql
+++ models/Practise/birthdate.sql
@@ -1,7 +1,7 @@
 with birthdate as(
     select Prescribing_doctor as Doctor,
      AGE , 
-     {{ ref('dob')}} as dob
+     {{ ref('dob') }} as dob
      from {{ source('fivetran_database_dbt_default_schema', 'CLEANSED_PRESCRIPTION_PRACTISE') }}
 ),
 
